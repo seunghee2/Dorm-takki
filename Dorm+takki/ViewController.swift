d90bf36fdb873f425cb914b8236134f7a588fa81
seunghee2 <smilagain07@daum.net>
2017-04-07 16:39:03 +0900

    init

---
+++ Dorm+takki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dorm+takki
+//
+//  Created by 이승희 on 07/04/2017.
+//  Copyright © 2017 이승희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
