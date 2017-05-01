9c633a3667641d33ed9e74b3b3b56455ff6252d0
seunghee2 <smilagain07@daum.net>
2017-05-01 21:07:49 +0900

    add my page

--- Dorm+takki/MyPageViewController.swift
+++ Dorm+takki/MyPageViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+class MyPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sections = [String]()
+    var titleForSection1 = [String]()
+    var titleForSection2 = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.sections = ["설정", "기타"]
+        self.titleForSection1 = ["개인정보 설정", "알림 설정"]
+        self.titleForSection2 = ["버그리포트", "개발자정보"]
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
         self.navigationItem.title = "MY PAGE"
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +37,39 @@ class MyPageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return titleForSection1.count
+        } else {
+            return titleForSection2.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if indexPath.section == 0 {
+            cell.textLabel?.text = self.titleForSection1[indexPath.row]
+        } else {
+            cell.textLabel?.text = self.titleForSection2[indexPath.row]
+        }
+        return cell
+    }
 
     /*
     // MARK: - Navigation
