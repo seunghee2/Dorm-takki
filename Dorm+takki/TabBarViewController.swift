988fd07e16d529b20a711f90977da626bc455230
seunghee2 <smilagain07@daum.net>
2017-05-08 15:15:00 +0900

    add ui files

--- Dorm+takki/TabBarViewController.swift
+++ Dorm+takki/TabBarViewController.swift
@@ -13,12 +13,10 @@ class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
@@ -33,16 +31,5 @@ class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
         */
         return true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
